e571500b8ecf37f50d7f2fb31031a5c35b108884
Rakindu Athukorala <rakinduathukorala@gmail.com>
2026-02-17 22:36:19 +0530

    Appointment service CRUD and schedule integration completed

---
+++ BACKEND/Appointment_Service/Appointment_Service/src/test/java/com/hospitalManagement/Appointment_Service/AppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitalManagement.Appointment_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
