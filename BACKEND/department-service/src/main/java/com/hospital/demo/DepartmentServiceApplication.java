e34c398554fc0e0d69bdd373f04720fc41520c50
kawshika pathum <140903656+kawshikapathum@users.noreply.github.com>
2026-02-19 10:43:47 +0530

    Add department-service with CRUD operations

---
+++ BACKEND/department-service/src/main/java/com/hospital/demo/DepartmentServiceApplication.java
@@ -0,0 +1,12 @@
+package com.hospital.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentServiceApplication {  // ← මෙක class එක
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepartmentServiceApplication.class, args);
+    }
+}
