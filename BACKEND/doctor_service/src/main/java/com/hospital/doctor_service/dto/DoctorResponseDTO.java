452b3a696a55280e01dd368b9acc65f559403dd5
Yashodha Rathnayaka <136618724+YashodhaRathnayaka1@users.noreply.github.com>
2026-02-27 13:41:25 +0530

    Create DoctorResponseDTO

---
+++ BACKEND/doctor_service/src/main/java/com/hospital/doctor_service/dto/DoctorResponseDTO.java
@@ -0,0 +1,43 @@
+package com.hospital.doctor_service.dto;
+
+public class DoctorResponseDTO {
+
+    private Long id;
+    private String name;
+    private String specialization;
+    private DepartmentDTO department;
+
+    // getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public DepartmentDTO getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(DepartmentDTO department) {
+        this.department = department;
+    }
+}
