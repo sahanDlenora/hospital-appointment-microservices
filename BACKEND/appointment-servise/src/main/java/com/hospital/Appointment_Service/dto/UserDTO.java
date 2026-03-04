33e0e016946b7fc0539dcc33661ef51f37f426a6
Rakindu Athukorala <rakinduathukorala@gmail.com>
2026-03-05 03:40:56 +0530

    update microservises

---
+++ BACKEND/appointment-servise/src/main/java/com/hospital/Appointment_Service/dto/UserDTO.java
@@ -0,0 +1,13 @@
+package com.hospitalManagement.Appointment_Service.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+
+    private Long id;
+    private String name;
+    private Integer age;
+    private String phone;
+    private String address;
+}
