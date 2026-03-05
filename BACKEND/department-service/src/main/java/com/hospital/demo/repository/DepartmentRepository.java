c0ffc575e6d2f09a2be4fb15c1c33797984e6eb1
kawshika pathum <140903656+kawshikapathum@users.noreply.github.com>
2026-03-05 17:10:50 +0530

    update

--- BACKEND/department-service/src/main/java/com/hospital/demo/repository/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hospital.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.hospital.demo.entity.Department;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
