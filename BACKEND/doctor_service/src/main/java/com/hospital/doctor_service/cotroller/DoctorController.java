d8978da841680add5ea17be8aaea371e8b3c3b67
Yashodha Rathnayaka <136618724+YashodhaRathnayaka1@users.noreply.github.com>
2026-03-06 09:12:58 +0530

    Update DoctorController
    Added CrossOrigin

--- BACKEND/doctor_service/src/main/java/com/hospital/doctor_service/cotroller/DoctorController.java
+++ BACKEND/doctor_service/src/main/java/com/hospital/doctor_service/cotroller/DoctorController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 // Controller for handling Doctor-related API requests
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/doctors")
 public class DoctorController {
