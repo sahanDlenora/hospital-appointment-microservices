d1c8c549a6beeddb7af6e84096ca74f796a8be2c
Rakindu Athukorala <rakinduathukorala@gmail.com>
2026-03-06 14:29:04 +0530

    update to main
    appointment service

---
+++ BACKEND/appointment_service/src/main/java/com/hospital/appointment_service/repository/AppointmentRepository.java
@@ -0,0 +1,10 @@
+package com.hospital.appointment_service.repository;
+
+import com.hospital.appointment_service.model.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment,Long> {
+
+}
